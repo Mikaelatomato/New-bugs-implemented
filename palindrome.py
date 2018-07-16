c7b39456938787c9abb39e4f64539300b42cb778
Jean Alvarez <soukgames@gmail.com>
2018-07-16 11:03:47 -0500

    Revert "is a (word) a palindrome?"
    This reverts commit 088833c76a939799e7e9c4c471864490b2e09b7a.

--- palindrome.py
+++
@@ -1,8 +0,0 @@
-def palindrome():
-    word = str(input("Write a word to know if it's palindrome: "))
-    possiblepal = word[::-1]
-    if word[::] == possiblepal:
-        return print("the given word is a palindrome")
-    else:
-        return print("the given word isn't a palindrome")
-palindrome()
