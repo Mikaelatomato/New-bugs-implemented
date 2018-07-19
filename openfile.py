b9b9747d2e589034768af9a0b16bcba706091c1b
Jean Alvarez <soukgames@gmail.com>
2018-07-19 18:16:50 -0500

    opening files, writing on them

---
+++ openfile.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'http://www.nytimes.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+
+filename = str(input("how could you like to call the file?: ")) + ".txt"
+
+with open(filename, 'w') as open_file:
+    for story_heading in soup.find_all(class_="story-heading"):
+        if story_heading.a:
+           open_file.write(story_heading.a.text.replace("\n", " ").strip())
+        else:
+            open_file.write(story_heading.contents[0].strip())
